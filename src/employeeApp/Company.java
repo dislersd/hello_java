0028474572941b365849df8a66ba056dd27df28c
dislersd <dislersd@gmail.com>
2020-06-27 16:27:37 -0700

    added id to Company method

--- src/employeeApp/Company.java
+++ src/employeeApp/Company.java
@@ -4,12 +4,14 @@ public class Company {
   private static int maxId = 0;
 
   public final static double match401k = 0.05;
-  
+
   public int id;
   public String name;
   public int debt;
 
   public Company(String name, int debt) {
+    maxId++;
+    id = maxId;
     this.name = name;
     this.debt = debt;
   }
