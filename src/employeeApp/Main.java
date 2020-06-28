164997f301c259aff2db5a4d6cfb54003137a189
dislersd <dislersd@gmail.com>
2020-06-28 07:57:12 -0700

    working with numbers

--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -13,11 +13,20 @@ public class Main {
     Employee emp4 = new Employee("Allison", "O'Donnel", 80000, true, c2.id, h2.getId());
 
     System.out.println("*** Query Data");
-    System.out.println(c1);
+    System.out.println(c1.toString());
+    System.out.println(c1); // by default calls the toString method same as line above
     System.out.println();
     System.out.println("Original: " + c1.debt);
     c1.debt = -c1.debt;
     c1.debt = -c1.debt;
+
+    System.out.println("\n *** Working with numbers");
+    System.out.println("divide by 5 " + (c1.debt / 5));
+    System.out.println("mod 2 " + (c1.debt % 2));
+    int theAns = c1.debt / 7;
+    System.out.println("divide by 7 " + theAns);
+    double theAns2 = c1.debt / 7.0;
+    System.out.println("divide 7 double " + theAns2);
   }
 
   public static void main(String[] args) {
