6541d290be9a16990170436a263609ab678c3dec
dislersd <dislersd@gmail.com>
2020-06-30 13:02:04 -0700

    invoking horse methods in main program

--- src/transport/Main.java
+++ src/transport/Main.java
@@ -2,6 +2,17 @@ package transport;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Running");
+    System.out.println("\n *** Working with interfaces");
+
+    Horse seabiscut = new Horse("Seabiscut");
+    Horse affirmed = new Horse("Affirmed");
+    Horse joe = new Horse("Joe");
+
+    seabiscut.eat(10);
+    affirmed.eat(25);
+    seabiscut.move();
+    seabiscut.move();
+    seabiscut.move();
+    System.out.println("Seabiscut " + seabiscut.getFuelLevel());
   }
 }
