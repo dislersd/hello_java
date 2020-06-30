de3cb4e3c731e62b17bf475c36bed15e980c2f3a
dislersd <dislersd@gmail.com>
2020-06-30 15:42:49 -0700

    extended HorseFromVehicle class from AbstractVehicle class

---
+++ src/transport/HorseFromVehicle.java
@@ -0,0 +1,29 @@
+package transport;
+
+public class HorseFromVehicle extends AbstractVehicle {
+  private String name;
+
+  public HorseFromVehicle(String name, int fuel) {
+    super(fuel);
+    this.name = name;
+  }
+
+  public HorseFromVehicle(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String getPath() {
+    return "Grass";
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return "HorseFromVehicle: name: " + name + " fuel " + fuel;
+  }
+}
